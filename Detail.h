8d91d040bb8dd978f6759c3f123bb89550e6ed65
g1sus <proyectos_technology@hotmail.com>
2015-02-04 01:38:10 -0600

    Terminando ViewControllers

---
+++ Detail.h
@@ -0,0 +1,13 @@
+//
+//  Detail.h
+//  Lab03
+//
+//  Created by Jesus Antonio Hernandez Mendez on 04/02/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Detail : UIViewController
+
+@end
