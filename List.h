62f63f4f081046d0d9151387a637963a93ad9cc8
g1sus <proyectos_technology@hotmail.com>
2015-02-04 01:19:50 -0600

    Creando View Controllers

---
+++ List.h
@@ -0,0 +1,13 @@
+//
+//  Animo.h
+//  Lab03
+//
+//  Created by Jesus Antonio Hernandez Mendez on 04/02/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface List : UIViewController
+
+@end
---
+++ Share.h
@@ -0,0 +1,13 @@
+//
+//  Share.h
+//  Lab03
+//
+//  Created by Jesus Antonio Hernandez Mendez on 04/02/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Share : UIViewController
+
+@end
